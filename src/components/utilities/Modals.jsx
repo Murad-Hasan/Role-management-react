ea2fee24a77f5bca8771239244185bfe199bfcbf
Al Mahamud Murad <muradhasan.7845@gmail.com>
2022-01-11 01:16:12 +0600

    add function

--- src/components/utilities/Modals.jsx
+++ src/components/utilities/Modals.jsx
@@ -1,11 +1,31 @@
-import React from "react";
-import getRolePermissionData from "../../services/roles/RolePermissionData";
+import React, { useState } from "react";
+import getRoleMasterData from "../../services/roles/RoleMasterData.js";
 import getUserData from "../../services/users/UserData";
 import PopUp from "./PopUp";
 
 const Modals = ({ togglePopup, isOpen }) => {
-  const user = getUserData();
-  const role = getRolePermissionData();
+  const users = getUserData();
+  const roles = getRoleMasterData();
+
+  const [user, setUser] = useState("");
+  const [role, setRole] = useState("");
+
+  const changeUser = (e) => {
+    setUser(e.target.value);
+  };
+  const changeRole = (e) => {
+    setRole(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (user === "" && role == "") {
+      alert("Please select a user and role");
+    } else {
+      console.log("user" + user, "role" + role);
+      togglePopup();
+    }
+  };
 
   return (
     <div>
@@ -26,9 +46,19 @@ const Modals = ({ togglePopup, isOpen }) => {
                   <div>
                     <label className="text-black">
                       <span className="text-black">Name: </span>
-                      <select className="text-black border rounded-md outline-none border-slate-400 text-center">
-                        {user.map((user, index) => (
-                          <option key={index}>{user.name}</option>
+                      <select
+                        value={user}
+                        onChange={changeUser}
+                        required
+                        className="text-black border rounded-md outline-none border-slate-400 text-center"
+                      >
+                        <option value="Please Select a user">
+                          Please Select a user
+                        </option>
+                        {users.map((user, index) => (
+                          <option value={JSON.stringify(user)} key={index}>
+                            {user.name}
+                          </option>
                         ))}
                       </select>
                     </label>
@@ -36,9 +66,19 @@ const Modals = ({ togglePopup, isOpen }) => {
                   <div>
                     <label className="text-black">
                       <span className="text-black">Role: </span>
-                      <select className="text-black border outline-none rounded-md border-slate-400 text-center">
-                        {role.map((role, index) => (
-                          <option key={index}>{role.name}</option>
+                      <select
+                        value={role}
+                        onChange={changeRole}
+                        required
+                        className="text-black border outline-none rounded-md border-slate-400 text-center"
+                      >
+                        <option value="Please select a role">
+                          Please select a role
+                        </option>
+                        {roles.map((role, index) => (
+                          <option value={JSON.stringify(role)} key={index}>
+                            {role.name}
+                          </option>
                         ))}
                       </select>
                     </label>
@@ -47,6 +87,7 @@ const Modals = ({ togglePopup, isOpen }) => {
                 <div className="flex justify-center mt-4">
                   <button
                     type="submit"
+                    onClick={handleSubmit}
                     className="px-2 py-1 shadow-violet-50  font-medium active:scale-95 bg-green-400 rounded-md"
                   >
                     Save
